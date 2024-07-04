a84bfde48d3120e9a88321a01e481fae56f35ce4
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-07-04 16:47:30 +0200

    Modified

---
+++ src/main/java/se/lexicon/Snacks.java
@@ -0,0 +1,22 @@
+package se.lexicon;
+
+public class Snacks extends Product {
+
+    public Snacks(double price, String snack) {
+        super(price, snack);
+    }
+
+    @Override
+    public String examine() {
+        return "{ id: " + getId() + ", snack:" + getProductName() + " } ";
+
+    }
+
+    @Override
+
+
+    }
+}
+
+
+
