66cd17abee9dd7e46f282c62ca0a6f7481d3c2bf
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-07-04 18:20:28 +0200

    modified

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,13 +1,22 @@
 package se.lexicon;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+public class App {
+    public static void main(String[] args) {
+        Product[] products = {
+                new drinks("Fanta", 1L,50),
+                new drinks("Srite", 1L,50),
+                new drinks("Coca-Cola", 1L,50),
+                new drinks("Mango", 1L,65),
+                new drinks("Orange", 1L,65),
+                new drinks("Apple", 1L,65),
+                new Snacks(30, "Chocolate"),
+                new Snacks(30, "Gnuts"),
+                new Snacks(30, "Chips"),
+                new Snacks(45, "Muffin"),
+
+
+        };
+
     }
 }
