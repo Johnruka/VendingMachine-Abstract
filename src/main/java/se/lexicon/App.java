502e9f2dd7525cbe62c0fbcef5c5dc6db4c7cbcd
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-07-04 18:29:44 +0200

    modified

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -5,7 +5,7 @@ public class App {
     public static void main(String[] args) {
         Product[] products = {
                 new drinks("Fanta", 1L, 50),
-                new drinks("Srite", 1L, 50),
+                new drinks("Sprite", 1L, 50),
                 new drinks("Coca-Cola", 1L, 50),
                 new drinks("Mango", 1L, 65),
                 new drinks("Orange", 1L, 65),
