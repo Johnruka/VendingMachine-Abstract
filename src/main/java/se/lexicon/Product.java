ee5678508ba5b39c434207039d58f768d1519a4d
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-07-04 16:57:16 +0200

    modified

---
+++ src/main/java/se/lexicon/Product.java
@@ -0,0 +1,42 @@
+package se.lexicon;
+
+public abstract class Product {
+
+    private static int sequencer = 0;
+    private final int id;
+    private double price;
+    private String productName;
+
+    public Product(double price, String productName) {
+        this.id = ++sequencer;
+        this.price = price;
+        this.productName = productName;
+    }
+
+
+
+    public abstract String examine();
+
+    /**
+     * Abstract method to use the product.
+     */
+
+    public abstract String use();
+
+
+    public int getId() {
+        return id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+
+}
+
+
