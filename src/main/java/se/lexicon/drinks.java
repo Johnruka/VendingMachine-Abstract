cb8e73080b6111fcf95bf31e046510f499327069
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-07-04 16:46:32 +0200

    Modified

---
+++ src/main/java/se/lexicon/drinks.java
@@ -0,0 +1,20 @@
+package se.lexicon;
+
+public class drinks extends Product{
+
+    private final int quantity;
+
+    public drinks(String productName, int quantity, double price) {
+        super(price, productName);
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String examine() {
+        return "{ id: " + getId() + ", drinks:" + getProductName() + "with" + "quantity}";
+    }
+
+    @Override
+
+    }
+}
